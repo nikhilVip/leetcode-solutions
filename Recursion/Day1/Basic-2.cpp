87e3d52f9063f39e55dd4eac104ef55f32cf51f5
nikhilVip <105498741+nikhilVip@users.noreply.github.com>
2023-02-23 18:48:53 +0530

    Update Basic-2.cpp

--- Recursion/Day1/Basic-2.cpp
+++ Recursion/Day1/Basic-2.cpp
@@ -19,3 +19,26 @@ int main()
 
     return 0;
 }
+// one more way 
+#include <iostream>
+
+using namespace std;
+void fun(int i,int n){
+    if(i<1){
+        return ;
+      
+    }
+   
+        fun(i-1,n);
+           cout<<i <<endl;
+        
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    fun(n,n);
+
+    return 0;
+}
