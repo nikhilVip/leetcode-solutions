da28883da9176c249ddd3def820784fe8885e661
nikhilVip <105498741+nikhilVip@users.noreply.github.com>
2023-02-25 18:17:33 +0530

    Create combination sum.cpp

---
+++ Recursion/Day2/combination sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+void findcombination(int ind,int target,vector<int>&arr,vector<vector<int>>&ans,vector<int>&ds){
+    if(ind==arr.size()){
+        if(target==0){
+            ans.push_back(ds);
+        }
+        return ;
+    }
+    if(arr[ind]<=target){
+        ds.push_back(arr[ind]);
+        findcombination(ind,target-arr[ind],arr,ans,ds);
+        ds.pop_back();
+    }
+    findcombination(ind+1,target,arr,ans,ds);
+}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        findcombination(0,target,candidates,ans,ds);
+        return ans;
+    }
+};
+//tc= 2^t*k
+//sc=k*x// hypothetical sc
